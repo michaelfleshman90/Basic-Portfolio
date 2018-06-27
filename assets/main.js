cf9a0213ec9a23d848f830f3e0ed641e36f4a9e5
Michael Fleshman <michaelfleshman90@gmail..com>
2018-06-27 18:26:19 -0400

    button function

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+function aboutbutton() {
+    window.location.href="about.html";
+}
+function contactbutton() {
+    window.location.href="contact.html";
+}
+function portfoliobutton() {
+    window.location.href="portfolio.html";
+}
